d76af794e6cb65e4370a0346a803c1bb3bbbb9ad
Marc Battistello <marc.battistello@gmail.com>
2018-02-01 09:00:46 -0600

    commit 3

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gladys-module-helloworld
 This is a demo hello world module to explore building custom modules for Gladys Project.
+
+New text here.....
--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 
 module.exports = function( sails ) {
 
+	const install = require( './lib/install.js' );
+	
+	gladys.on( 'ready', function(){
+		init().catch( sails.log.warn );
+		});
+
 
 	function sayHello(){ return 'hello'; }
 
 	return {
-		sayHello: sayHello
+		sayHello: sayHello,
+		install: install,
+		setup: install
 		};
 
 };
